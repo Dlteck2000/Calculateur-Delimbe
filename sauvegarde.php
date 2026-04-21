6775fece4cd2439722f85dd6ed49ddf2f228008e
Damien Loisel <dlteck2000@gmail.com>
2026-04-21 15:21:39 +0200

    Restore comments and PHP code in sauvegarde.php

--- sauvegarde.php
+++ sauvegarde.php
@@ -1,11 +1,11 @@
+<?php
 /**
  * Calculateur de réglage pour épandeurs Delimbe T24/T28
  * Auteur : Damien Loisel (Dlteck2000)
  * Licence : GNU General Public License v3.0 (GPL-3.0)
  * Année : 2026
  */
- 
- <?php
+
 $json_data = file_get_contents('php://input');
 if ($json_data) {
     file_put_contents('reglages.json', $json_data);
